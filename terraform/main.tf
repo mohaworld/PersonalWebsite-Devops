74789573197b4284622ad743e0120642d0a5e702
Mohamed Alaa <mohaworld@gmail.com>
2026-03-25 20:23:53 +0200

    changing bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,47 +2,47 @@ resource "aws_s3_bucket" "static-site" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.static-site.id
+#resource "aws_s3_bucket_website_configuration" "website_config" {
+#  bucket = aws_s3_bucket.static-site.id
 
-  index_document {
-    suffix = "index.html"
-  }
+#  index_document {
+#    suffix = "index.html"
+#  }
 
-  error_document {
-    key = "error.html"
-  }
+#  error_document {
+#    key = "error.html"
+#  }
 
-}
+#}
 
 resource "aws_s3_bucket_public_access_block" "static_site_public_access" {
   bucket = aws_s3_bucket.static-site.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 
 }
 
-resource "aws_s3_bucket_policy" "static_site_policy" {
-  bucket = aws_s3_bucket.static-site.id
+#resource "aws_s3_bucket_policy" "static_site_policy" {
+#  bucket = aws_s3_bucket.static-site.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.static-site.arn}/*"
-      }
-    ]
-  })
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Effect    = "Allow"
+#        Principal = "*"
+#        Action    = "s3:GetObject"
+#        Resource  = "${aws_s3_bucket.static-site.arn}/*"
+#      }
+#    ]
+#  })
 
-  depends_on = [aws_s3_bucket_public_access_block.static_site_public_access]
+#  depends_on = [aws_s3_bucket_public_access_block.static_site_public_access]
 
-}
+#}
 
 
 
