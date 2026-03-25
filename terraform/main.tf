7180f47abdf67ae9de9f3a2cd2b018a1eec8d5a6
Mohamed Alaa <mohaworld@gmail.com>
2026-03-25 14:29:20 +0200

    configure website

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,43 @@
 resource "aws_s3_bucket" "static-site" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.static-site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static-site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static-site.arn}/*"
+      }
+    ]
+  })
+
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site_public_access" {
+  bucket = aws_s3_bucket.static-site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
